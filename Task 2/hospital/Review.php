0a91363a117c879f45344f0cf4dbfda996d9bcf2
Ahmed Ashraf Ahmed <89888246+ahmeddashraff@users.noreply.github.com>
2022-09-09 22:09:50 +0200

    survey values are held

--- Task 2/hospital/Review.php
+++ Task 2/hospital/Review.php
@@ -1,4 +1,16 @@
 <?php     include "layouts/session.php";
+
+$total = 0;
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST)
+{
+    foreach($_POST as $surveyValues)
+    {
+        $total += (int)$surveyValues;
+    }
+    $_SESSION['result'] = $total;
+}
+
  ?>
 
 <!doctype html>
@@ -32,43 +44,43 @@
 
                 <tr>
                     <th scope="row">Are you satisfied with the level of cleanliness?</th>
-                    <td><input type="radio" id="cleanliness" name="cleanliness"></td>
-                    <td><input type="radio" id="cleanliness" name="cleanliness"></td>
-                    <td><input type="radio" id="cleanliness" name="cleanliness"></td>
-                    <td><input type="radio" id="cleanliness" name="cleanliness"></td>
+                    <td><input type="radio" id="cleanliness1" name="cleanliness" value=0 checked></td>
+                    <td><input type="radio" id="cleanliness2" name="cleanliness" value=3></td>
+                    <td><input type="radio" id="cleanliness3" name="cleanliness" value=5></td>
+                    <td><input type="radio" id="cleanliness4" name="cleanliness" value=10></td>
                 </tr>
                 <tr>
                     <th scope="row">Are you satisified with the service prices?</th>
-                    <td><input type="radio" id="service_price" name="service_price"></td>
-                    <td><input type="radio" id="service_price" name="service_price"></td>
-                    <td><input type="radio" id="service_price" name="service_price"></td>
-                    <td><input type="radio" id="service_price" name="service_price"></td>
+                    <td><input type="radio" id="service_price1" name="service_price" value=0 checked></td>
+                    <td><input type="radio" id="service_price2" name="service_price" value=3></td>
+                    <td><input type="radio" id="service_price3" name="service_price" value=5></td>
+                    <td><input type="radio" id="service_price4" name="service_price" value=10></td>
                 </tr>
                 <tr>
                     <th scope="row">Are you satisfied with the nursing service?</th>
-                    <td><input type="radio" id="nursing_service" name="nursing_service"></td>
-                    <td><input type="radio" id="nursing_service" name="nursing_service"></td>
-                    <td><input type="radio" id="nursing_service" name="nursing_service"></td>
-                    <td><input type="radio" id="nursing_service" name="nursing_service"></td>
+                    <td><input type="radio" id="nursing_service1" name="nursing_service" value=0 checked></td>
+                    <td><input type="radio" id="nursing_service2" name="nursing_service" value=3></td>
+                    <td><input type="radio" id="nursing_service3" name="nursing_service" value=5></td>
+                    <td><input type="radio" id="nursing_service4" name="nursing_service" value=10></td>
                 </tr>
                 <tr>
                     <th scope="row">Are satisfied with the level of the doctors?</th>
-                    <td><input type="radio" id="doctor_level" name="doctor_level"></td>
-                    <td><input type="radio" id="doctor_level" name="doctor_level"></td>
-                    <td><input type="radio" id="doctor_level" name="doctor_level"></td>
-                    <td><input type="radio" id="doctor_level" name="doctor_level"></td>
+                    <td><input type="radio" id="doctor_level1" name="doctor_level" value=0 checked></td>
+                    <td><input type="radio" id="doctor_level2" name="doctor_level" value=3></td>
+                    <td><input type="radio" id="doctor_level3" name="doctor_level" value=5></td>
+                    <td><input type="radio" id="doctor_level4" name="doctor_level" value=10></td>
                 </tr>
                 <tr>
                     <th scope="row">Are you satisfied with the calmness in the hospital?</th>
-                    <td><input type="radio" id="calmness" name="calmness"></td>
-                    <td><input type="radio" id="calmness" name="calmness"></td>
-                    <td><input type="radio" id="calmness" name="calmness"></td>
-                    <td><input type="radio" id="calmness" name="calmness"></td>
+                    <td><input type="radio" id="calmness1" name="calmness" value=0 checked></td>
+                    <td><input type="radio" id="calmness2" name="calmness" value=3></td>
+                    <td><input type="radio" id="calmness3" name="calmness" value=5></td>
+                    <td><input type="radio" id="calmness4" name="calmness" value=10></td>
                 </tr>
             </tbody>
         </table>
 
-        <button class = "btn btn-dark w-100">submit</button>
+        <button class="btn btn-dark w-100">Go to Result</button>
     </form>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
